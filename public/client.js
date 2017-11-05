1f713023ae1cdd67b71175184bc8bfd033fca1d2
David Germain <davidispizza@gmail.com>
2017-11-05 00:41:59 -0400

    Draw houses to screen

--- public/client.js
+++ public/client.js
@@ -2,10 +2,30 @@ $(document).ready(() => {
 
   var socket = io();
   var canvas = $('#screen')[0];
-  var ctx = canvas.getContext('2d');
+  var context = canvas.getContext('2d');
 
   socket.on('setup', (houses) => {
-    console.log(houses);
+    clearCanvas();
+    drawHouses(houses);
   })
+
+  function drawHouses(houses) {
+    console.log(houses);
+    for (var i = 0; i < houses.length; i++) {
+      var house = houses[i];
+      drawHouse(house);
+    }
+  }
+
+  function clearCanvas() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function drawHouse(house) {
+    context.beginPath();
+    context.fillStyle = house.color;
+    var position = house.position;
+    context.fillRect(position.x, position.y, house.WIDTH, house.HEIGHT);
+  }
   
 });
