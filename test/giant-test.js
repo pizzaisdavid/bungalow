434be31245a6de587e8c71de36548b6b9f8bb957
Clickity-Clack <spaceman.bob@live.com>
2017-12-01 12:40:36 -0500

    added giant tests

--- test/giant-test.js
+++ test/giant-test.js
@@ -3,40 +3,57 @@ var should = require('chai').should()
 
 describe('giant: ', () => {
   it('should have a rightShape property', () => {
-    let sut = new Giant({}, {})
+    let giant = new Giant({}, {})
 
-    sut.should.have.property('rightShape').a('object')
+    giant.should.have.property('rightShape').a('object')
   })
 
   it('should have a leftShape property', () => {
-    let sut = new Giant({}, {})
+    let giant = new Giant({}, {})
 
-    sut.should.have.property('leftShape').a('object')
+    giant.should.have.property('leftShape').a('object')
   })
 
   describe('constructor: ', () => {
     it('first parameter should be assigned to rightShape', () => {
       let right = {}
       let left = {}
-      let sut = new Giant(right, left)
+      let giant = new Giant(right, left)
 
-      sut.rightShape.should.be.same(right)
+      giant.rightShape.should.be.same(right)
+    })
+
+    it('currentControl should be the same as right', () => {
+      let right = new DummyShape(40, 40)
+      let left = new DummyShape(40, 40)
+      let giant = new Giant(right, left)
+
+      giant.rightShape.should.be.same(right)
+      giant.currentControl.should.be.same(right)
     })
   })
 
   describe('do', () => {
     it('should have a do function', () => {
-      let sut = new Giant({}, {})
+      let giant = new Giant({}, {})
 
-      sut.should.have.property('do').a('function')
+      giant.should.have.property('do').a('function')
     })
 
-    it('should move the right shape up when you press space', () => {
-      let right = {}
-      let left = {}
-      let sut = new Giant(right, left)
+    it('should move the right shape up at \'UP\' command when rightShape is currentControl', () => {
+      let right = new DummyShape(40, 40)
+      let left = new DummyShape(40, 40)
+      let giant = new Giant(right, left)
 
-      sut.rightShape.should.be.same(right)
+      giant.rightShape
+      giant.do('UP')
     })
   })
+
+  class DummyShape{
+    constructor(width, height){
+      this.x = width - 40
+      this.y = height - 40
+    }
+  }
 })
